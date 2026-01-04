b763e721c17507a3401b6dbc23af8f08e2248b10
rsessa <rsessa1@gmail.com>
2026-01-04 05:53:31 +0100

    add import RecentCards

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -23,6 +23,7 @@ import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
+import RecentCards from "./RecentCards"
 
 export {
   ArticleTitle,
